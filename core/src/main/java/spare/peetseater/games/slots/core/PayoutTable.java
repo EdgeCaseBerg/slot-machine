c189afa4d8bad10aed84226dd7b565e0c2ffc1bc
peetseater <peetseater@gmail.com>
2025-10-27 19:03:26 -0400

    Payout table for 1 coin

--- core/src/main/java/spare/peetseater/games/slots/core/PayoutTable.java
+++ core/src/main/java/spare/peetseater/games/slots/core/PayoutTable.java
@@ -1,5 +1,6 @@
 package spare.peetseater.games.slots.core;
 
+import java.util.Arrays;
 import java.util.Hashtable;
 import java.util.Map;
 
@@ -14,7 +15,49 @@ public class PayoutTable {
 
     public int payoutFor(String firstSymbol, String secondSymbol, String thirdSymbol) {
         String key = keyFor(firstSymbol, secondSymbol, thirdSymbol);
-        return payoutTable.getOrDefault(key, 0);
+        if (payoutTable.containsKey(key)) {
+            return payoutTable.get(key);
+        }
+
+        String[] row = new String[]{firstSymbol, secondSymbol, thirdSymbol};
+        boolean allSevens = Arrays.stream(row).allMatch(symbolNameMap::isSeven);
+        if (allSevens) {
+            payoutTable.put(key, 80);
+            return payoutTable.get(key);
+        }
+
+        boolean redWhiteAndBlue = symbolNameMap.isRed(firstSymbol) && symbolNameMap.isWhite(secondSymbol) && symbolNameMap.isBlue(thirdSymbol);
+        if (redWhiteAndBlue) {
+            payoutTable.put(key, 20);
+            return payoutTable.get(key);
+        }
+
+        boolean allBars = Arrays.stream(row).allMatch(symbolNameMap::isBar);
+        if (allBars) {
+            payoutTable.put(key, 5);
+            return payoutTable.get(key);
+        }
+
+        boolean allReds = Arrays.stream(row).allMatch(symbolNameMap::isRed);
+        if (allReds) {
+            payoutTable.put(key, 2);
+            return payoutTable.get(key);
+        }
+
+        boolean allWhite = Arrays.stream(row).allMatch(symbolNameMap::isWhite);
+        if (allWhite) {
+            payoutTable.put(key, 2);
+            return payoutTable.get(key);
+        }
+
+        boolean allBlue = Arrays.stream(row).allMatch(symbolNameMap::isBlue);
+        if (allBlue) {
+            payoutTable.put(key, 2);
+            return payoutTable.get(key);
+        }
+
+        payoutTable.put(key, 0);
+        return payoutTable.get(key);
     }
 
     protected void loadPayoutTable(SymbolNameMap symbolNameMap) {
@@ -22,7 +65,30 @@ public class PayoutTable {
         payoutTable.put(
             keyFor(symbolNameMap.getRedSeven(), symbolNameMap.getWhiteSeven(), symbolNameMap.getBlueSeven()), 2400
         );
-        // TODO data entry
+        payoutTable.put(
+            keyFor(symbolNameMap.getRedSeven(), symbolNameMap.getRedSeven(), symbolNameMap.getRedSeven()), 1200
+        );
+        payoutTable.put(
+            keyFor(symbolNameMap.getWhiteSeven(), symbolNameMap.getWhiteSeven(), symbolNameMap.getWhiteSeven()), 200
+        );
+        payoutTable.put(
+            keyFor(symbolNameMap.getBlueSeven(), symbolNameMap.getBlueSeven(), symbolNameMap.getBlueSeven()), 150
+        );
+        payoutTable.put(
+            keyFor(symbolNameMap.getOneBar(), symbolNameMap.getTwoBar(), symbolNameMap.getThreeBar()), 50
+        );
+        payoutTable.put(
+            keyFor(symbolNameMap.getThreeBar(), symbolNameMap.getThreeBar(), symbolNameMap.getThreeBar()), 40
+        );
+        payoutTable.put(
+            keyFor(symbolNameMap.getTwoBar(), symbolNameMap.getTwoBar(), symbolNameMap.getTwoBar()), 25
+        );
+        payoutTable.put(
+            keyFor(symbolNameMap.getOneBar(), symbolNameMap.getOneBar(), symbolNameMap.getOneBar()), 10
+        );
+        payoutTable.put(
+            keyFor(symbolNameMap.getBlank(), symbolNameMap.getBlank(), symbolNameMap.getBlank()), 1
+        );
         this.payoutTable = payoutTable;
     }
 
