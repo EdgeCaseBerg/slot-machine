85e98df704222d22d2d1b380fcf29235c7a390ac
peetseater <peetseater@gmail.com>
2025-10-29 16:46:43 -0400

    Add test for spinning reels with rng seed

--- core/src/test/java/spare/peetseater/games/slots/core/SlotMachineTest.java
+++ core/src/test/java/spare/peetseater/games/slots/core/SlotMachineTest.java
@@ -1,5 +1,6 @@
 package spare.peetseater.games.slots.core;
 
+import com.badlogic.gdx.math.MathUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -30,4 +31,22 @@ class SlotMachineTest {
         // Initial reels are at 2 bar 2 bar 2 bar
         assertEquals(25, initialAll0Payout);
     }
+
+    @Test
+    void spin() {
+        MathUtils.random.setSeed(1);
+        slotMachine.spin();
+        String spin1Reel1 = slotMachine.getFirstReel().getCurrentSymbol();
+        String spin1Reel2 = slotMachine.getSecondReel().getCurrentSymbol();
+        String spin1Reel3 = slotMachine.getThirdReel().getCurrentSymbol();
+        MathUtils.random.setSeed(1);
+        slotMachine.spin();
+        String spin2Reel1 = slotMachine.getFirstReel().getCurrentSymbol();
+        String spin2Reel2 = slotMachine.getSecondReel().getCurrentSymbol();
+        String spin2Reel3 = slotMachine.getThirdReel().getCurrentSymbol();
+        assertEquals(spin1Reel1, spin2Reel1);
+        assertEquals(spin1Reel2, spin2Reel2);
+        assertEquals(spin1Reel3, spin2Reel3);
+    }
 }
+
