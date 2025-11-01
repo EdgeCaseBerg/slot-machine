dbdac28428baed6f5013473a1d6641c9141df2cb
peetseater <peetseater@gmail.com>
2025-11-01 18:29:25 -0400

    prototyping some placement code
    It's not right though. my ratios are slightly skewed and the math is
    once again eluding me

--- core/src/main/java/spare/peetseater/games/FirstScreen.java
+++ core/src/main/java/spare/peetseater/games/FirstScreen.java
@@ -1,17 +1,63 @@
 package spare.peetseater.games;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import spare.peetseater.games.slots.core.SymbolNameMap;
+
+import java.util.HashMap;
 
 /** First screen of the application. Displayed after the application is created. */
 public class FirstScreen implements Screen {
+
+    SpriteBatch batch;
+    OrthographicCamera camera;
+    Viewport viewport;
+    Texture[] textures = new Texture[6];
+    Texture machineMask;
+
     @Override
     public void show() {
-        // Prepare your screen here.
+        batch = new SpriteBatch();
+        camera = new OrthographicCamera();
+        viewport = new FitViewport(1280, 720, camera);
+        camera.setToOrtho(false);
+        camera.update();
+        textures[0] = new Texture(Gdx.files.internal("RedSeven.png"));
+        textures[1] = new Texture(Gdx.files.internal("WhiteSeven.png"));
+        textures[2] = new Texture(Gdx.files.internal("BlueSeven.png"));
+        textures[3] = new Texture(Gdx.files.internal("OneBar.png"));
+        textures[4] = new Texture(Gdx.files.internal("TwoBar.png"));
+        textures[5] = new Texture(Gdx.files.internal("ThreeBar.png"));
+        machineMask = new Texture(Gdx.files.internal("MachineMaskMini.png"));
     }
 
     @Override
     public void render(float delta) {
         // Draw your screen here. "delta" is the time since last render in seconds.
+        camera.update();
+        batch.setProjectionMatrix(camera.combined);
+        batch.begin();
+        float ratio = 16f/9f;
+        for (int i = 0; i < 3; i++) {
+            float x = 20 * 9 + 20 * 7 * i;
+            for (int j = 0; j < textures.length; j++) {
+                float y = 20 * 4 + j * 80;
+                batch.draw(textures[j], x * ratio, y * ratio, 80 * ratio, 80 * ratio);
+            }
+        }
+        batch.draw(machineMask, 0, 0, 1280, 720);
+        batch.end();
+
+        if (Gdx.input.isKeyPressed(Input.Keys.ESCAPE)) {
+            Gdx.app.exit();
+        }
     }
 
     @Override
