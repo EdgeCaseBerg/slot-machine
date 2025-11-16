1473c653dc8cc5c1ed752499cf65cca5189261e9
peetseater <peetseater@gmail.com>
2025-11-16 12:58:02 -0500

    Wallet class

---
+++ core/src/main/java/spare/peetseater/games/slots/core/Wallet.java
@@ -0,0 +1,33 @@
+package spare.peetseater.games.slots.core;
+
+public class Wallet {
+    private int funds;
+
+    public Wallet(int initial) {
+        funds = initial;
+    }
+
+    public void awardAmount(int toAdd) {
+        funds += toAdd;
+    }
+
+    public boolean hasEnoughToBet(int amount) {
+        return amount >= funds;
+    }
+
+    public boolean isBroke() {
+        return funds <= 0;
+    }
+
+    public void subtractAmount(int amount) {
+        funds -= amount;
+        // Unlike real life. There is no debt here.
+        if (funds < 0) {
+            funds = 0;
+        }
+    }
+
+    public int getFunds() {
+        return funds;
+    }
+}
