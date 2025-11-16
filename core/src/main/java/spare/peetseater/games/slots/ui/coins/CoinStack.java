4ec35e722e87da8b816f47306c293a3c46924067
peetseater <peetseater@gmail.com>
2025-11-16 15:33:43 -0500

    Coin Stacks to their own package
    and seperate the two classes out a bit

---
+++ core/src/main/java/spare/peetseater/games/slots/ui/coins/CoinStack.java
@@ -0,0 +1,43 @@
+package spare.peetseater.games.slots.ui.coins;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+class CoinStack {
+    private final Texture coinTexture;
+    private float topPosition;
+    private final float coinHeight;
+    private final float x;
+    private final float y;
+
+    public CoinStack(Texture coinTexture, float x, float y, float coinHeight) {
+        this.x = x;
+        this.y = y;
+        this.topPosition = y;
+        this.coinHeight = coinHeight;
+        this.coinTexture = coinTexture;
+    }
+
+    public boolean hasCoins() {
+        return this.y != topPosition;
+    }
+
+    public void addCoin() {
+        // TODO start coins at the top of y range and make them fall to
+        // the appropriate stack for a fun visual
+        this.topPosition += coinHeight;
+    }
+
+    public void subtractCoin() {
+        this.topPosition -= coinHeight;
+        if (this.topPosition < y) {
+            this.topPosition = y;
+        }
+    }
+
+    public void draw(SpriteBatch batch) {
+        for (float i = y; i < topPosition; i += coinHeight) {
+            batch.draw(coinTexture, x, i, 1, coinHeight);
+        }
+    }
+}
--- core/src/main/java/spare/peetseater/games/slots/ui/coins/CoinStacksDisplay.java
+++ core/src/main/java/spare/peetseater/games/slots/ui/coins/CoinStacksDisplay.java
@@ -1,4 +1,4 @@
-package spare.peetseater.games.slots.ui;
+package spare.peetseater.games.slots.ui.coins;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -10,14 +10,12 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class CoinStacksDisplay {
-    private final Texture coinTexture;
     private final Vector2 xRange;
     private final Vector2 yRange;
     private static final float coinHeight = 0.2f;
     public List<CoinStack> stacks;
 
     public CoinStacksDisplay(Texture coinTexture, Vector2 xRange, Vector2 yRange) {
-        this.coinTexture = coinTexture;
         this.xRange = xRange;
         this.yRange = yRange;
         this.stacks = new LinkedList<>();
@@ -25,15 +23,15 @@ public class CoinStacksDisplay {
         assert(xRange.x < xRange.y);
         assert(yRange.x < yRange.y);
         for (float x = xRange.x; x < xRange.y; x+= xStep) {
-            stacks.add(new CoinStack(x, yRange.x));
+            float yStart = yRange.x; // we're just treating the vector2 as a tuple
+            stacks.add(new CoinStack(coinTexture, x, yStart, coinHeight));
         }
+        assert(!stacks.isEmpty());
     }
 
     public void addCoins(int numberOfCoins) {
         for (int i = 0; i < numberOfCoins; i++) {
             CoinStack stack = stacks.get(MathUtils.random(0, stacks.size() - 1));
-            // TODO start coins at the top of y range and make them fall to
-            // the appropriate stack for a fun visual
             stack.addCoin();
         }
     }
@@ -43,6 +41,9 @@ public class CoinStacksDisplay {
             // Note that this likely will get inefficient in the long term.
             // We will deal with it another time.
             List<CoinStack> stacksWithCoins = stacks.stream().filter(CoinStack::hasCoins).collect(Collectors.toList());
+            if (stacksWithCoins.isEmpty()) {
+                return;
+            }
             CoinStack stack = stacksWithCoins.get(MathUtils.random(0, stacksWithCoins.size() - 1));
             stack.subtractCoin();
         }
@@ -51,39 +52,6 @@ public class CoinStacksDisplay {
     public void draw(SpriteBatch batch) {
         for (CoinStack stack : stacks) {
             stack.draw(batch);
-        }
-    }
-
-    protected class CoinStack {
-        private float topPosition;
-        private final float x;
-        private final float y;
-
-        public CoinStack(float x, float y) {
-            this.x = x;
-            this.y = y;
-            this.topPosition = y;
-        }
-
-        public boolean hasCoins() {
-            return this.y != topPosition;
-        }
-
-        public void addCoin() {
-            this.topPosition += coinHeight;
-        }
-
-        public void subtractCoin() {
-            this.topPosition -= coinHeight;
-            if (this.topPosition < y) {
-                this.topPosition = y;
-            }
-        }
-
-        public void draw(SpriteBatch batch) {
-            for (float i = y; i < topPosition; i+= coinHeight) {
-                batch.draw(coinTexture, x, i, 1, coinHeight);
-            }
         }
     }
 
